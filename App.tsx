f6f15007f97f99f718e05c5c3b36c2aae4ae5600
MuradMaestro <murada@maestro.st>
2023-10-08 09:13:20 +0400

    App changes

--- App.tsx
+++ App.tsx
@@ -1,53 +1,33 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
+import * as React from 'react';
 
-import React from 'react';
-import type {PropsWithChildren} from 'react';
-import RNFS from "react-native-fs"
 import {
-  Image,
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
   StyleSheet,
-  Text,
-  useColorScheme,
   View,
+  Platform,
+  TouchableHighlight,
+  Text,
 } from 'react-native';
-import {ArViewerView} from 'react-native-ar-viewer';
-import {Platform} from 'react-native';
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+import { ArViewerView } from 'react-native-ar-viewer';
+import RNFS from 'react-native-fs';
 
-
-
-
-function App(): JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-  // var RNFS = require('react-native-fs');
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
+export default function App() {
   const [localModelPath, setLocalModelPath] = React.useState<string>();
   const [showArView, setShowArView] = React.useState(true);
   const ref = React.useRef() as React.MutableRefObject<ArViewerView>;
 
   const loadPath = async () => {
-    const modelSrc ='https://github.com/riderodd/react-native-ar/blob/main/example/src/dice.glb?raw=true'
-
-    const modelPath = `${RNFS.DocumentDirectoryPath}/model.glb`;
+    const modelSrc =
+      Platform.OS === 'android'
+        ? 'https://github.com/MuradMaestro/Hackathon/blob/main/assets/human_skull_high_detailed (1).glb?raw=true'
+        : 'https://github.com/riderodd/react-native-ar/blob/main/example/src/dice.usdz?raw=true';
+    const modelPath = `${RNFS.DocumentDirectoryPath}/model.${
+      Platform.OS === 'android' ? 'glb' : 'usdz'
+    }`;
     console.log("modelPath",modelPath);
     
     const exists = await RNFS.exists(modelPath);
+    console.log("exists",exists);
+    
     if (!exists) {
       await RNFS.downloadFile({
         fromUrl: modelSrc,
@@ -57,42 +37,99 @@ function App(): JSX.Element {
 
     setLocalModelPath(modelPath);
   };
-  
 
   React.useEffect(() => {
     loadPath();
-  },[]);
+  });
 
-  console.log(localModelPath);
-  
-  // console.log(require("./assets/salam.png"));
+  const takeSnapshot = () => {
+    ref.current?.takeScreenshot().then(async (base64Image) => {
+      const date = new Date();
+      const filePath = `${
+        RNFS.CachesDirectoryPath
+      }/arscreenshot-${date.getFullYear()}-${date.getMonth()}-${date.getDay()}-${date.getHours()}-${date.getMinutes()}-${date.getSeconds()}.jpg`;
+      await RNFS.writeFile(filePath, base64Image, 'base64');
+      console.log('Screenshot written to ' + filePath);
+    });
+  };
+
+  const reset = () => {
+    ref.current?.reset();
+  };
+
+  const rotateX = () => {
+    ref.current?.rotate(0, 25, 0);
+  };
+  const rotateY = () => {
+    ref.current?.rotate(0, 0, 25);
+  };
+
+  const mountUnMount = () => setShowArView(!showArView);
+
+
 
   return (
-    <SafeAreaView style={styles.sectionContainer}>
-      <ArViewerView
-        style={{flex: 1}}
-        model={localModelPath}
-        lightEstimation
-        manageDepth
-        allowRotate
-        allowScale
-        allowTranslate
-        disableInstantPlacement
-        onStarted={() => console.log('started')}
-        onEnded={() => console.log('ended')}
-        onModelPlaced={() => console.log('model displayed')}
-        onModelRemoved={() => console.log('model not visible anymore')}
-        planeOrientation="both"
-      />
-      {/* <Image source={require("./assets/salam.png")}></Image> */}
-    </SafeAreaView>
+    <View style={styles.container}>
+      {localModelPath && showArView && (
+        <ArViewerView
+          model={localModelPath}
+          style={styles.arView}
+
+          disableInstantPlacement
+          manageDepth
+          allowRotate
+          allowScale
+          allowTranslate
+          onStarted={() => console.log('started')}
+          onEnded={() => console.log('ended')}
+          onModelPlaced={() => console.log('model displayed')}
+          onModelRemoved={() => console.log('model not visible anymore')}
+          ref={ref}
+
+        />
+      )}
+      <View style={styles.footer}>
+        <TouchableHighlight onPress={takeSnapshot} style={styles.button}>
+          <Text>Take Snapshot</Text>
+        </TouchableHighlight>
+        <TouchableHighlight onPress={mountUnMount} style={styles.button}>
+          <Text>{showArView ? 'Unmount' : 'Mount'}</Text>
+        </TouchableHighlight>
+        <TouchableHighlight onPress={reset} style={styles.button}>
+          <Text>Reset</Text>
+        </TouchableHighlight>
+        <TouchableHighlight onPress={rotateX} style={styles.button}>
+          <Text>RotateX</Text>
+        </TouchableHighlight>
+        <TouchableHighlight onPress={rotateY} style={styles.button}>
+          <Text>RotateX</Text>
+        </TouchableHighlight>
+      </View>
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
-  sectionContainer: {
-   flex:1
-  }
+  container: {
+    flex: 1,
+    backgroundColor: 'black',
+  },
+  arView: {
+    flex: 2,
+  },
+  footer: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    flexWrap: 'nowrap',
+    flexDirection: 'row',
+    backgroundColor: 'black',
+  },
+  button: {
+    borderColor: 'black',
+    borderWidth: 1,
+    backgroundColor: 'black',
+    padding: 10,
+    margin: 5,
+  },
 });
-
-export default App;
